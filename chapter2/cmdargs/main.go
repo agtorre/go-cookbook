1289f4357a73b0dcece245b792d9351ddc005aa0
Aaron Torres <tcboox@gmail.com>
2017-01-08 14:22:08 -0800

    command args section written with tests

---
+++ chapter2/cmdargs/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	c := MenuConf{}
+	menu := c.SetupMenu()
+	menu.Parse(os.Args[1:])
+
+	// we use arguments to switch between commands
+	// flags are also an argument
+	if len(os.Args) > 1 {
+		// we don't care about case
+		switch strings.ToLower(os.Args[1]) {
+		case "version":
+			c.Version()
+		case "greet":
+			f := c.GetSubMenu()
+			if len(os.Args) < 3 {
+				f.Usage()
+				return
+			}
+			if len(os.Args) > 3 {
+				f.Parse(os.Args[3:])
+			}
+			c.Greet(os.Args[2])
+
+		default:
+			fmt.Println("Invalid command")
+			menu.Usage()
+			return
+		}
+	} else {
+		menu.Usage()
+		return
+	}
+}
