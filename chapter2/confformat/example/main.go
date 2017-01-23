b1285df1ac295f2148f87e09bcc99cf5cc85739f
Aaron Torres <tcboox@gmail.com>
2017-01-22 18:28:58 -0800

    cleaning up large files

--- chapter2/confformat/example/main.go
+++ chapter2/confformat/example/main.go
@@ -1,96 +1,17 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/agtorre/go-cookbook/chapter2/confformat"
-)
-
-const (
-	exampleTOML = `name="Example1"
-age=99
-    `
-
-	exampleJSON = `{"name":"Example2","age":98}`
-
-	exampleYAML = `name: Example3
-age: 97    
-    `
-)
-
-func marshalAll() error {
-	t := confformat.TOMLData{
-		Name: "Name1",
-		Age:  20,
-	}
-
-	j := confformat.JSONData{
-		Name: "Name2",
-		Age:  30,
-	}
-
-	y := confformat.YAMLData{
-		Name: "Name3",
-		Age:  40,
-	}
-
-	tomlRes, err := t.ToTOML()
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("TOML Marshal =", tomlRes.String())
-
-	jsonRes, err := j.ToJSON()
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("JSON Marshal=", jsonRes.String())
-
-	yamlRes, err := y.ToYAML()
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("YAML Marshal =", yamlRes.String())
-	return nil
-}
-
-func unmarshalAll() error {
-	t := confformat.TOMLData{}
-	j := confformat.JSONData{}
-	y := confformat.YAMLData{}
-
-	if _, err := t.Decode([]byte(exampleTOML)); err != nil {
-		return err
-	}
-	fmt.Println("TOML Unmarshal =", t)
-
-	if err := j.Decode([]byte(exampleJSON)); err != nil {
-		return err
-	}
-	fmt.Println("JSON Unmarshal =", j)
-
-	if err := y.Decode([]byte(exampleYAML)); err != nil {
-		return err
-	}
-	fmt.Println("Yaml Unmarshal =", y)
-	return nil
-}
+import "github.com/agtorre/go-cookbook/chapter2/confformat"
 
 func main() {
-
-	if err := marshalAll(); err != nil {
+	if err := confformat.MarshalAll(); err != nil {
 		panic(err)
 	}
 
-	if err := unmarshalAll(); err != nil {
+	if err := confformat.UnmarshalAll(); err != nil {
 		panic(err)
 	}
 
 	if err := confformat.OtherJSONExamples(); err != nil {
 		panic(err)
 	}
-
 }
--- chapter2/flags/flags.go
+++ chapter2/flags/flags.go
@@ -3,39 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"strconv"
-	"strings"
 )
-
-// CountTheWays is a custom type that
-// we'll read a flag into
-type CountTheWays []int
-
-func (c *CountTheWays) String() string {
-	result := ""
-	for _, v := range *c {
-		if len(result) > 0 {
-			result += " ... "
-		}
-		result += fmt.Sprint(v)
-	}
-	return result
-}
-
-// Set will be used by the flag package
-func (c *CountTheWays) Set(value string) error {
-	values := strings.Split(value, ",")
-
-	for _, v := range values {
-		i, err := strconv.Atoi(v)
-		if err != nil {
-			return err
-		}
-		*c = append(*c, i)
-	}
-
-	return nil
-}
 
 // Config will be the holder for our flags
 type Config struct {
@@ -76,16 +44,4 @@ func (c *Config) GetMessage() string {
 
 	msg = fmt.Sprintf("%s with a certainty of %d out of 10. Let me count the ways %s", msg, c.howAwesome, c.countTheWays.String())
 	return msg
-}
-
-func main() {
-	// initialize our setup
-	c := Config{}
-	c.Setup()
-
-	// generally call this from main
-	flag.Parse()
-
-	fmt.Println(c.GetMessage())
-
 }
