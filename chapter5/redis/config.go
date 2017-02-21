37ce141acee0a4c6030dc20e8c54af9ebc0648a3
Aaron Torres <tcboox@gmail.com>
2017-02-20 19:05:37 -0800

    added redis examples

---
+++ chapter5/redis/config.go
@@ -0,0 +1,19 @@
+package redis
+
+import (
+	"os"
+
+	redis "gopkg.in/redis.v5"
+)
+
+// Setup initializes a redis client
+func Setup() (*redis.Client, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: os.Getenv("REDISPASSWORD"),
+		DB:       0, // use default DB
+	})
+
+	_, err := client.Ping().Result()
+	return client, err
+}
