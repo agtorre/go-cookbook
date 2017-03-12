278099a459979fdfe778005671d704f6db4d2e14
Aaron Torres <tcboox@gmail.com>
2017-03-12 15:08:49 -0700

    Added a section on content negotiation

---
+++ chapter7/negotiate/negotiate.go
@@ -0,0 +1,27 @@
+package negotiate
+
+import (
+	"net/http"
+
+	"github.com/unrolled/render"
+)
+
+// Negotiator wraps render and does
+// some switching on ContentType
+type Negotiator struct {
+	ContentType string
+	*render.Render
+}
+
+// GetNegotiator takes a request, and figures
+// out the ContentType from the Content-Type header
+func GetNegotiator(r *http.Request) *Negotiator {
+	contentType := r.Header.Get("Content-Type")
+
+	n := Negotiator{
+		ContentType: contentType,
+		Render:      render.New(),
+	}
+
+	return &n
+}
