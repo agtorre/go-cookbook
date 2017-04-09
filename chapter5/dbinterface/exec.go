9f00b7447f786ea2b22a1ab6bfbb8a65c6272f5d
Aaron Torres <tcboox@gmail.com>
2017-04-09 12:33:09 -0700

    added dbinterface testing

--- chapter5/dbinterface/exec.go
+++ chapter5/dbinterface/exec.go
@@ -1,33 +1,19 @@
 package dbinterface
 
-import "github.com/agtorre/go-cookbook/chapter5/database"
-
 // Exec replaces the Exec from the previous
 // recipe
-func Exec() error {
-	db, err := database.Setup()
-	if err != nil {
-		return err
-	}
+func Exec(db DB) error {
 
-	tx, err := db.Begin()
-	if err != nil {
-		return err
-	}
-	// this wont do anything if commit is successful
-	defer tx.Rollback()
 	// uncaught error on cleanup, but we always
 	// want to cleanup
 	defer db.Exec("DROP TABLE example")
 
-	if err := Create(tx); err != nil {
+	if err := Create(db); err != nil {
 		return err
 	}
 
-	if err := Query(tx); err != nil {
+	if err := Query(db); err != nil {
 		return err
 	}
-
-	err = tx.Commit()
-	return err
+	return nil
 }
